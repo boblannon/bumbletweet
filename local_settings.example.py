d473ea884eff0bb1b4ebe46a3f616e15490091d3
Bob Lannon <blannon@gmail.com>
2013-10-30 16:24:02 -0400

    example of local settings

---
+++ local_settings.example.py
@@ -0,0 +1,11 @@
+# Go to http://dev.twitter.com and create an app. 
+# The consumer key and secret will be generated for you after
+consumer_key="YOUR-KEY-HERE"
+consumer_secret="YOUR-KEY-SECRET-HERE"
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+access_token="YOUR-TOKEN-HERE"
+access_token_secret="YOUR-TOKEN-SECRET-HERE"
+
+
